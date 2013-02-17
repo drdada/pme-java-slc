5937e78efe7caf7c6ea5231208c8b36228e63886
thibaud.ecarot@gmail.com <thibaud.ecarot@gmail.com@ba455e6d-1b9b-9536-0260-cfa18b8dc5cf>
2013-02-17 22:31:16 +0000

    Connexion base

--- Sonson/src/org/sonson/dal/Dal.java
+++ Sonson/src/org/sonson/dal/Dal.java
@@ -1,7 +1,9 @@
 package org.sonson.dal;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
@@ -11,13 +13,23 @@ import java.sql.Statement;
  */
 public final class Dal {
 	private static volatile Dal instance = null;
-	// A voir pour voklatile sur Connection
-	private static volatile Connection connect = null;
-	private static Statement statement = null;
-	private static ResultSet resultSet = null;
+	
+	private Connection connect = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
 	
 	private Dal() {
         super();
+        
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connect = DriverManager.getConnection("jdbc:mysql://akrogames.com/push?user=push&password=azf1234");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
     }
 	
 	public final static Dal getInstance() {
--- Sonson/src/org/sonson/main/Sonson.java
+++ Sonson/src/org/sonson/main/Sonson.java
@@ -1,11 +1,14 @@
 package org.sonson.main;
 
+import org.sonson.dal.Dal;
+
 /**
  * Controller class for this application Sonson
  * @author Matteo D'Addamio & Arnaud Booms (Henallux 2012 2013)
  *
  */
 public class Sonson extends AbstractApplication {
+	private Dal dal;
 	
 	public Sonson() {
 		
@@ -14,5 +17,8 @@ public class Sonson extends AbstractApplication {
 	
 	public void launch() {
 		logger.info("Launch Sonson Application");
+		
+		logger.info("Initialisation couche DAL");
+		this.dal = Dal.getInstance();
 	}
 }
