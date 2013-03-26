d6a00bc6508b33e72f0fbd59aa5c352054e0aa9b
booms.arnaud@gmail.com <booms.arnaud@gmail.com@ba455e6d-1b9b-9536-0260-cfa18b8dc5cf>
2013-03-26 11:15:42 +0000

    Fenetre de connexion

--- ProjetExam/src/gui/Connexion.java
+++ ProjetExam/src/gui/Connexion.java
@@ -10,21 +10,29 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Connexion extends JPanel {
-private JLabel connexionLabel;
+private JLabel connexionLabel,log,pwd,test;
 private JTextField login;
 private JPasswordField pass;
-private JButton ok;	
+private JButton ok,annuler;	
 
 	public Connexion(){
-		this.setLayout(new GridLayout(4,1));
-		connexionLabel = new JLabel("Connexion\nVeuillez rentrer les informations de connexion de la base de donnée");
+		this.setLayout(new GridLayout(4,2));
+		connexionLabel = new JLabel("Veuillez rentrer les informations de connexion à la base de données");
+		log = new JLabel("Login");
+		test = new JLabel("\n");
+		pwd = new JLabel("Password");
 		login = new JTextField("Login");
 		pass = new JPasswordField();
-		ok = new JButton("OK");
+		ok = new JButton("Ok");
+		annuler = new JButton("Annuler");
 		this.add(connexionLabel);
+		this.add(test);
+		this.add(log);
 		this.add(login);
+		this.add(pwd);
 		this.add(pass);
 		this.add(ok);
+		this.add(annuler);
 	}
 	
 
