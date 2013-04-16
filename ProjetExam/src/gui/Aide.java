9d4ab94518e1ef0b4a7680e43084f3178f0a1069
daddamio.matteo@gmail.com <daddamio.matteo@gmail.com@ba455e6d-1b9b-9536-0260-cfa18b8dc5cf>
2013-04-16 07:40:10 +0000

    modification de la classe Aide

--- ProjetExam/src/gui/Aide.java
+++ ProjetExam/src/gui/Aide.java
@@ -8,32 +8,34 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Aide extends JPanel{
 	private JLabel texte;
 	private JButton retour;
 	private Container cont;
 	public Aide(Container cont){
-		this.setLayout(new BorderLayout());
-		this.cont = cont;
-		RetourListener retourL = new RetourListener();
-		retour = new JButton("Retour");
+		this.setLayout(new BorderLayout()); //On défini le gestionnaire de tracé
+		this.cont = cont; //On obtient le container
+		RetourListener retourL = new RetourListener(); //Création de l'objet retourL (Listener)
+		retour = new JButton("Retour"); //Création de l'objet jbutton retour
 		texte = new JLabel("<html><body><center>" +
 				"Programme réalisé par Mr Arnaud Booms et Mr Matteo D'Addamio<br>" +
 				"Dans le cadre du cours de java de deuxieme TI<br>" +
 				"Henallux 2012-2013<br>" +
-				" </center></body></html>");
-		this.add(texte,BorderLayout.CENTER);
-		this.add(retour,BorderLayout.SOUTH);
-		retour.addActionListener(retourL);
+				" </center></body></html>"); //On crée le jlabel
+		texte.setHorizontalAlignment(SwingConstants.CENTER); //On centre le jlabel
+		this.add(texte,BorderLayout.CENTER); //On ajoute le texte au centre du jpanel
+		this.add(retour,BorderLayout.SOUTH); //On ajoute le bouton retour dans le fond du jpanel
+		retour.addActionListener(retourL); //On défini l'objet listener (action qui serra exécutée quand on clique)
 	}
 	private class RetourListener implements ActionListener {
 		public void actionPerformed (ActionEvent e){
-			Accueil acc = new Accueil();
-			cont.removeAll();
-			cont.add(acc);
-			cont.repaint();
-			cont.validate();
+			Accueil acc = new Accueil(); //On crée l'objet accueil
+			cont.removeAll(); //On supprime tout dans le container
+			cont.add(acc); //On ajoute le jpanel accueil dans le container
+			cont.repaint(); //On raffraichis
+			cont.validate(); //Ici aussi sinon obligé de redimentionner pour voir les modifications
 		}
 	}
 }
