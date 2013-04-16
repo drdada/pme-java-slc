bda14f1c7edf7a6f03e5ea285595bcab2cf9e720
booms.arnaud@gmail.com <booms.arnaud@gmail.com@ba455e6d-1b9b-9536-0260-cfa18b8dc5cf>
2013-04-16 07:47:41 +0000

    Statut connexion DB

--- ProjetExam/src/gui/Accueil.java
+++ ProjetExam/src/gui/Accueil.java
@@ -1,12 +1,38 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.sql.SQLException;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import main.Projet;
 
 public class Accueil extends JPanel{
-	private JLabel texte;
+	private JLabel texte,connecter,deconnecter;
 	public Accueil(){
-		texte = new JLabel("<html><body><center><h1>TITRE</h1><p>Texte ici</p></center></body></html>");
+		this.setLayout(new GridLayout(2,1));
+		texte = new JLabel("<html><body><center><h1>Projet Java</h1><h2>Suivi de maintenance du matériel Informatique (Série 13)</h2><p>Voici le résultat de notre dur labeur</p></center></body></html>");
+		connecter = new JLabel("Vous etes connecter à la DB.");
+		deconnecter = new JLabel("Vous n'etes pas connecter à la DB !!!");
 		this.add(texte); //ajoute le jlabel dans le panel
+		texte.setHorizontalAlignment(SwingConstants.CENTER);
+
+		try {
+			// si l'objet connexion est vide OU qu'il est fermé
+			if(Projet.getConnexion() == null || Projet.getConnexion().isClosed()){
+				deconnecter.setHorizontalAlignment(SwingConstants.CENTER);
+				this.add(deconnecter);
+			}// sinon
+			else{
+				connecter.setHorizontalAlignment(SwingConstants.CENTER);
+				this.add(connecter);
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
