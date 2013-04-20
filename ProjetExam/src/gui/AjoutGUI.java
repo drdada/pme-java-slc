899f1bdbdc7d649fbec51411781afa8a0b7ba722
booms.arnaud@gmail.com <booms.arnaud@gmail.com@ba455e6d-1b9b-9536-0260-cfa18b8dc5cf>
2013-04-20 20:25:10 +0000

    Modif ajout intervention

--- ProjetExam/src/gui/AjoutGUI.java
+++ ProjetExam/src/gui/AjoutGUI.java
@@ -7,6 +7,7 @@ import java.util.Vector;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import main.Projet;
@@ -14,26 +15,131 @@ import dal.AccessBDGen;
 
 public class AjoutGUI extends JPanel {
 	private JComboBox fournisseur,intervention,pc;
-	private JLabel lDateSignalement,lDescriptifbref,lSignaleur,lPreneurEnCharge,lEtatInterv,lSuiviViaFournisseur,lDateContact,lDatePrise,lDateRetour,lEtatRetour,lDateRemiseService,lTempsInterne,lResultat,lFkPcUnit,lFkTypeInterv,lFkFournisseurIntervenant;
-	
+	private JLabel lDateSignalement,lDescriptifbref,lSignaleur,lPreneurEnCharge,lEtatInterv,lSuiviViaFournisseur,lDateContact,lDatePrise,lDateRetour,lEtatRetour,lDateRemiseService,lTempsInterne,lResultat,lFkPcUnit,lFkTypeInterv,lFkFournisseurIntervenant,lNoInterv;
+	private JTextField tfNoInterv,tfSignaleur,tfPreneurEnCharge,tfSuiviViaFournisseur,tfTempsInterne;
 	
 	public AjoutGUI(){
 		
 		this.setLayout(new GridLayout(7,2,5,5));
 		buildJlabel();
 		buildJcomboBox();
+		buildJTextField();
 		
+		this.add(lNoInterv);
+		this.add(tfNoInterv);
+		this.add(lDateSignalement);
+		
+		this.add(lDescriptifbref);
+		
+		this.add(lSignaleur);
+		this.add(tfSignaleur);
+		this.add(lPreneurEnCharge);
+		this.add(tfPreneurEnCharge);
+		this.add(lEtatInterv);
+		
+		this.add(lSuiviViaFournisseur);
+		this.add(tfSuiviViaFournisseur);
+		this.add(lDateContact);
+		
+		this.add(lDatePrise);
+		
+		this.add(lDateRetour);
+		
+		this.add(lEtatRetour);
+		
+		this.add(lDateRemiseService);
+		
+		this.add(lTempsInterne);
+		this.add(tfTempsInterne);
+		this.add(lResultat);
+		this.add(lFkFournisseurIntervenant);
 		this.add(fournisseur);
+		this.add(lFkTypeInterv);
 		this.add(intervention);
+		this.add(lFkPcUnit);
 		this.add(pc);
+	}
+	private void buildJTextField(){
+		tfNoInterv = new JTextField("NoInterv");
+		tfNoInterv.setEditable(false);
+		
+		tfPreneurEnCharge = new JTextField();
+		
+		tfSignaleur = new JTextField();
+		
+		tfSuiviViaFournisseur = new JTextField();
+		
+		tfTempsInterne = new JTextField();
 	}
 	
 	private void buildJlabel(){
-		lDateSignalement = new JLabel("Date Signalement"); //On crée l'objet jlabel
-		lDateSignalement.setToolTipText("Date du document signalant le probléme"); //On met un petit texte d'aide
+		lDateContact = new JLabel("Date Signalement Fournisseur"); //On crée l'objet jlabel
+		lDateContact.setToolTipText("Date de signalement au fournisseur"); //On met un petit texte d'aide
+		lDateContact.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lDatePrise = new JLabel("Date Prise Matériel"); //On crée l'objet jlabel
+		lDatePrise.setToolTipText("Date où le matériel à été emporté"); //On met un petit texte d'aide
+		lDatePrise.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lDateRemiseService = new JLabel("Date Remise En Service"); //On crée l'objet jlabel
+		lDateRemiseService.setToolTipText("Date de la remise en service"); //On met un petit texte d'aide
+		lDateRemiseService.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lDateRetour = new JLabel("Date Retour Fournisseur"); //On crée l'objet jlabel
+		lDateRetour.setToolTipText("Date de retour de chez le fournisseur"); //On met un petit texte d'aide
+		lDateRetour.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lDateSignalement = new JLabel("Date Du Signalement"); //On crée l'objet jlabel
+		lDateSignalement.setToolTipText("Date du document signalant le problème"); //On met un petit texte d'aide
 		lDateSignalement.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
 		
+		lDescriptifbref= new JLabel("Descriptif Problème"); //On crée l'objet jlabel
+		lDescriptifbref.setToolTipText("Bref descriptif du problème"); //On met un petit texte d'aide
+		lDescriptifbref.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
 		
+		lEtatInterv = new JLabel("Etat Intervention"); //On crée l'objet jlabel
+		lEtatInterv.setToolTipText("Etat actuel de l'intervention"); //On met un petit texte d'aide
+		lEtatInterv.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lEtatRetour = new JLabel("Etat Retour"); //On crée l'objet jlabel
+		lEtatRetour.setToolTipText("Etat lors du retour"); //On met un petit texte d'aide
+		lEtatRetour.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lFkFournisseurIntervenant = new JLabel("Fournisseur"); //On crée l'objet jlabel
+		lFkFournisseurIntervenant.setToolTipText("Référence vers le fournisseur"); //On met un petit texte d'aide
+		lFkFournisseurIntervenant.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lFkPcUnit = new JLabel("PC"); //On crée l'objet jlabel
+		lFkPcUnit.setToolTipText("Référence vers le PC objet de l'intervention"); //On met un petit texte d'aide
+		lFkPcUnit.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lFkTypeInterv = new JLabel("Type Intervention"); //On crée l'objet jlabel
+		lFkTypeInterv.setToolTipText("Référence vers le type de l'intervention"); //On met un petit texte d'aide
+		lFkTypeInterv.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lNoInterv = new JLabel("Numéro Intervention"); //On crée l'objet jlabel
+		lNoInterv.setToolTipText("Numéro incrémentiel de l'intervention en cours"); //On met un petit texte d'aide
+		lNoInterv.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lPreneurEnCharge = new JLabel("Administrateur"); //On crée l'objet jlabel
+		lPreneurEnCharge.setToolTipText("Administrateur prenant en charge"); //On met un petit texte d'aide
+		lPreneurEnCharge.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lResultat = new JLabel("Résultat"); //On crée l'objet jlabel
+		lResultat.setToolTipText("Résultat de l'intervention"); //On met un petit texte d'aide
+		lResultat.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lSignaleur = new JLabel("Signaleur Incident"); //On crée l'objet jlabel
+		lSignaleur.setToolTipText("Nom de la personne signalante"); //On met un petit texte d'aide
+		lSignaleur.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lSuiviViaFournisseur = new JLabel("Suivi Via Fournisseur"); //On crée l'objet jlabel
+		lSuiviViaFournisseur.setToolTipText("Renvoyé ou non au fournisseur"); //On met un petit texte d'aide
+		lSuiviViaFournisseur.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
+		
+		lTempsInterne = new JLabel("Temps Presté"); //On crée l'objet jlabel
+		lTempsInterne.setToolTipText("Temps Presté en interne pour l'intervention"); //On met un petit texte d'aide
+		lTempsInterne.setHorizontalAlignment(SwingConstants.RIGHT); //On aligne le jlabel a gauche
 	}
 	
 	private void buildJcomboBox(){
